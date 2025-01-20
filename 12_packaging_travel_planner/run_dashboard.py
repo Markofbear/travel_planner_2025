c4ad3ab885c33020c2f8719f35b9a3479d9f8163
Markofbear <lagerblad.bjorn@gmail.com>
2025-01-20 10:52:58 +0100

    fixed req.txt created a run_dashboard for easier research

---
+++ 12_packaging_travel_planner/run_dashboard.py
@@ -0,0 +1,10 @@
+import subprocess
+from pathlib import Path
+
+def run_dashboard():
+    dashboard_path = Path(__file__).parents[0] / "frontend" / "dashboard.py"
+
+    subprocess.run(["streamlit", "run", str(dashboard_path)])
+
+if __name__ == "__main__":
+    run_dashboard()
